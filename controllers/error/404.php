d8da06eeb7ad101fd2c10d755545e0954243b2cb
Marin Milotić <mGitM101@gmail.com>
2024-06-11 19:04:09 +0200

    napravio raouting
    bez htaccess filea nema routinga, dodao routing file u root i unutar controllers foldera dodao home, listings i nestao 404 unutar error foldera

---
+++ controllers/error/404.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'This is an error page, WTF did you type, asshole?';
--- routes.php
+++ routes.php
@@ -1,2 +1,7 @@
 <?php
 
+return [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings.php',
+    '404' => 'controllers/error/404.php'
+];
