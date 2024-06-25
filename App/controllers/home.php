b62ff65a2a7ed9b3d5b8d7f512333b04e38b4824
Marin Milotić <mGitM101@gmail.com>
2024-06-25 12:16:52 +0200

    dodao dinamično potezanje listinga iz baze prema zadnjem ubačenom listingu

--- App/controllers/home.php
+++ App/controllers/home.php
@@ -3,7 +3,7 @@
 $config = require basePath('config/db.php');
 $db = new Database($config);
 
-$listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+$listings = $db->query('SELECT * FROM listings ORDER BY created_at DESC LIMIT 6')->fetchAll();
 
 loadView('home', [
     'listings' => $listings
--- App/controllers/listings.php
+++ App/controllers/listings.php
@@ -1,2 +1,10 @@
 <?php
-loadView('listings');
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$listings = $db->query('SELECT * FROM listings ORDER BY created_at DESC')->fetchAll();
+
+loadView('listings', [
+    'listings' => $listings
+]);
